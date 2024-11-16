14a34b71f522249caaf992334cd5e98e9f12cb3f
samarthbedare <bedaresamarth@gmail.com>
2024-11-16 12:35:04 +0530

    fix error for not importing the functions

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,6 +1,6 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { createOrUpdateUser } from '@/lib/actions/user'
+import { createOrUpdateUser, deleteUser } from '@/lib/actions/user'
 import { clerkClient } from '@clerk/nextjs/server'
 
 
