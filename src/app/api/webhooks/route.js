09ae539ecb6a1b3e2c79d52494941e7233c3cd19
samarthbedare <bedaresamarth@gmail.com>
2024-11-16 14:00:59 +0530

    fixed issues on mongodb 1

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,38 +1,39 @@
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { createOrUpdateUser, deleteUser } from '@/lib/actions/user'
-import { clerkClient } from '@clerk/nextjs/server'
-
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
+import { clerkClient } from '@clerk/nextjs/server';
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+    throw new Error(
+      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+    );
   }
 
   // Get the headers
-  const headerPayload = headers()
-  const svix_id = headerPayload.get('svix-id')
-  const svix_timestamp = headerPayload.get('svix-timestamp')
-  const svix_signature = headerPayload.get('svix-signature')
+  const headerPayload = headers();
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+  const svix_signature = headerPayload.get('svix-signature');
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400,
-    })
+    });
   }
 
   // Get the body
-  const payload = await req.json()
-  const body = JSON.stringify(payload)
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET)
+  const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt
+  let evt;
 
   // Verify the payload with the headers
   try {
@@ -40,26 +41,24 @@ export async function POST(req) {
       'svix-id': svix_id,
       'svix-timestamp': svix_timestamp,
       'svix-signature': svix_signature,
-    }) 
+    });
   } catch (err) {
-    console.error('Error verifying webhook:', err)
+    console.error('Error verifying webhook:', err);
     return new Response('Error occured', {
       status: 400,
-    })
+    });
   }
 
   // Do something with the payload
   // For this guide, you simply log the payload to the console
-  const { id } = evt?.data
-  const eventType = evt?.type
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
+  const { id } = evt?.data;
+  const eventType = evt?.type;
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log('Webhook body:', body);
 
   if (eventType === 'user.created' || eventType === 'user.updated') {
-    const { id, username, image_url, email_addresses, first_name, last_name } = evt?.data;
-    
-    
-
+    const { id, first_name, last_name, image_url, email_addresses, username } =
+      evt?.data;
     try {
       const user = await createOrUpdateUser(
         id,
@@ -67,25 +66,25 @@ export async function POST(req) {
         last_name,
         image_url,
         email_addresses,
-        username,
-      )
-
-      if (user && eventType==='user.created'){
+        username
+      );
+      if (user && eventType === 'user.created') {
         try {
           await clerkClient.users.updateUserMetadata(id, {
-            userMongoId: user._id,
-            isAdmin: user.isAdmin,
-          })
+            publicMetadata: {
+              userMongoId: user._id,
+              isAdmin: user.isAdmin,
+            },
+          });
         } catch (error) {
-          console.log('Error updating user metadata: ',error);
-          
+          console.log('Error updating user metadata:', error);
         }
       }
     } catch (error) {
-      console.log('Error creating or updating user metadata: ',error);
+      console.log('Error creating or updating user:', error);
       return new Response('Error occured', {
         status: 400,
-      })
+      });
     }
   }
 
@@ -97,9 +96,9 @@ export async function POST(req) {
       console.log('Error deleting user:', error);
       return new Response('Error occured', {
         status: 400,
-      })
+      });
     }
   }
 
-  return new Response('', { status: 200 })
+  return new Response('', { status: 200 });
 }
