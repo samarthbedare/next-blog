316df0c6555683e3cd60c1dd7c17f3c5440e1554
samarthbedare <bedaresamarth@gmail.com>
2024-11-17 18:17:08 +0530

    changes in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,9 @@
-import { auth, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)'
-]);
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
-export default clerkMiddleware(async (auth,req) => {
-  const { userId } = await auth()
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
   if (!userId && isProtectedRoute(req)) {
     return auth().redirectToSignIn();
   }
