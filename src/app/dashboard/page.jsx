1534f84afd17f2e7d6d48212b6f6627467fcf55d
samarthbedare <bedaresamarth@gmail.com>
2024-11-17 12:56:30 +0530

    complete the profile page in the dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+'use client';
 
-export default function DashboardPage() {
+import { useEffect, useState } from 'react';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
+import { useSearchParams } from 'next/navigation';
+import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
+import DashboardComp from '../components/DashboardComp';
+export default function Dashboard() {
+  const searchParams = useSearchParams();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(searchParams);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [searchParams]);
   return (
-    <div>DashboardPage</div>
-  )
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
+
+      {tab === 'posts' && <DashPosts />}
+
+      {tab === 'users' && <DashUsers />}
+      {tab === 'dash' && <DashboardComp />}
+    </div>
+  );
 }
