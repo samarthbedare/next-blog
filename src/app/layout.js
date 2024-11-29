ef7d204b9b99fe7005a20bbcd4cc35871a2b6994
samarthbedare <bedaresamarth@gmail.com>
2024-11-29 13:27:46 +0530

    added google verification html tag in post/[slug]/page.jsx

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,6 +29,7 @@ export default function RootLayout({ children }) {
       <html lang="en" suppressHydrationWarning>
         <head>
           <ThemeModeScript/>
+          <meta name="google-site-verification" content="jtGdDh2V26hDKP6fVGbjuzJ_f0WUlD-0E4DLZfKiReA" />
         </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
